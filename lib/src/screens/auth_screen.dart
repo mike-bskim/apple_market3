ea0151d742856da5b3b15f4b0a2de01836ea94f8
mike.bskim <mike.bskim@gmail.com>
2022-07-14 22:26:49 +0200

    06_pageview

--- lib/src/screens/auth_screen.dart
+++ lib/src/screens/auth_screen.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'start/intro_page.dart';
+
 class AuthScreen extends StatelessWidget {
   const AuthScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('AuthScreen ~~'),
+    return Scaffold(
+      body: PageView(
+        children: <Widget>[
+          const Center(
+            child: Text(
+              'AuthScreen',
+              style: TextStyle(color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          // const IntroPage(),
+          Container(color: Colors.accents[2]),
+          Container(color: Colors.accents[4]),
+        ],
       ),
     );
   }
