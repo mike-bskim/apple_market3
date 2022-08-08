82887dfd39989d584e971769619c1201f30d193a
mike.bskim <mike.bskim@gmail.com>
2022-08-08 15:33:14 +0200

    29_input_screen_layout1

--- lib/src/screens/input/input_screen.dart
+++ lib/src/screens/input/input_screen.dart
@@ -1,13 +1,13 @@
 // import 'package:extended_image/extended_image.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-// import 'package:beamer/beamer.dart';
 // import 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
 import 'package:get/get.dart';
 
-// import '../../constants/common_size.dart';
+import 'multi_image_select.dart';
+
+import '../../constants/common_size.dart';
 // import '../../utils/logger.dart';
-// import 'multi_image_select.dart';
 
 class InputScreen extends StatefulWidget {
   const InputScreen({Key? key}) : super(key: key);
@@ -17,13 +17,13 @@ class InputScreen extends StatefulWidget {
 }
 
 class _InputScreenState extends State<InputScreen> {
-  // final dividerCustom = Divider(
-  //   height: 1,
-  //   thickness: 1,
-  //   color: Colors.grey[350],
-  //   indent: padding_16,
-  //   endIndent: padding_16,
-  // );
+  final dividerCustom = Divider(
+    height: 1,
+    thickness: 1,
+    color: Colors.grey[350],
+    indent: padding_16,
+    endIndent: padding_16,
+  );
 
   // final underLineBorder =
   // const UnderlineInputBorder(borderSide: BorderSide(color: Colors.transparent));
@@ -34,14 +34,14 @@ class _InputScreenState extends State<InputScreen> {
   // final TextEditingController _titleController = TextEditingController();
   // final TextEditingController _detailController = TextEditingController();
 
-  @override
-  void dispose() {
-    // _priceController.dispose();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   // _priceController.dispose();
+  //   super.dispose();
+  // }
 
-  void attemptCreateItem() async {
-    if(FirebaseAuth.instance.currentUser == null) return;
+  // void attemptCreateItem() async {
+  //   if(FirebaseAuth.instance.currentUser == null) return;
     // 완료 버튼 클릭
     // Navigator.of(context).pop();
     // isCreatingItem = true;
@@ -78,8 +78,8 @@ class _InputScreenState extends State<InputScreen> {
 
     // await ItemService().createNewItem(itemModel, itemKey, userNotifier.user!.uid);
 
-    Get.back();
-  }
+    // Get.back();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -131,111 +131,112 @@ class _InputScreenState extends State<InputScreen> {
                 style: Theme.of(context).textTheme.headline6,
               ),
             ),
-            // body: ListView(
-            //   children: <Widget>[
-            //     // 멀티 이미지 영역
-            //     const MultiImageSelect(),
-            //     dividerCustom,
-            //     // 제목영역
-            //     TextFormField(
-            //       controller: _titleController,
-            //       decoration: InputDecoration(
-            //         hintText: '글제목',
-            //         contentPadding: const EdgeInsets.symmetric(horizontal: padding_16),
-            //         border: underLineBorder,
-            //         enabledBorder: underLineBorder,
-            //         focusedBorder: underLineBorder,
-            //       ),
-            //     ),
-            //     dividerCustom,
-            //     // 카타고리 영역
-            //     ListTile(
-            //       onTap: () {
-            //         debugPrint('/LOCATION_INPUT/LOCATION_CATEGORY_INPUT');
-            //         // context.beamToNamed('/$LOCATION_INPUT/$LOCATION_CATEGORY_INPUT');
-            //       },
-            //       dense: true,
-            //       title: const Text('카타고리'),
-            //       trailing: const Icon(Icons.navigate_next),
-            //     ),
-            //     dividerCustom,
-            //     Row(
-            //       children: <Widget>[
-            //         Expanded(
-            //           child: Padding(
-            //             padding: const EdgeInsets.only(left: padding_16),
-            //             child: TextFormField(
-            //               keyboardType: TextInputType.number,
-            //               controller: _priceController,
-            //               onChanged: (value) {
-            //                 if ('0 원' == value) {
-            //                   _priceController.clear();
-            //                 }
-            //                 setState(() {});
-            //               },
-            //               inputFormatters: [
-            //                 MoneyInputFormatter(mantissaLength: 0, trailingSymbol: ' 원')
-            //               ],
-            //               decoration: InputDecoration(
-            //                 hintText: '가격',
-            //                 prefixIcon: ImageIcon(
-            //                   const ExtendedAssetImageProvider('assets/imgs/won.png'),
-            //                   color:
-            //                   (_priceController.text.isEmpty) ? Colors.grey[350] : Colors.black87,
-            //                 ),
-            //                 prefixIconConstraints: const BoxConstraints(maxWidth: 20),
-            //                 contentPadding: const EdgeInsets.symmetric(vertical: padding_08),
-            //                 border: underLineBorder,
-            //                 enabledBorder: underLineBorder,
-            //                 focusedBorder: underLineBorder,
-            //               ),
-            //             ),
-            //           ),
-            //         ),
-            //         Padding(
-            //           padding: const EdgeInsets.only(right: padding_16),
-            //           child: TextButton.icon(
-            //             onPressed: () {
-            //               setState(() {
-            //                 _suggestPriceSelected = !_suggestPriceSelected;
-            //               });
-            //             },
-            //             icon: Icon(
-            //               _suggestPriceSelected ? Icons.check_circle : Icons.check_circle_outline,
-            //               color:
-            //               _suggestPriceSelected ? Theme.of(context).primaryColor : Colors.black54,
-            //             ),
-            //             label: Text(
-            //               '가격제안 받기',
-            //               style: TextStyle(
-            //                 color:
-            //                 _suggestPriceSelected ? Theme.of(context).primaryColor : Colors.black54,
-            //               ),
-            //             ),
-            //             style: TextButton.styleFrom(
-            //               backgroundColor: Colors.transparent,
-            //               primary: Colors.black45,
-            //             ),
-            //           ),
-            //         )
-            //       ],
-            //     ),
-            //     dividerCustom,
-            //     // 올릴 게시글 내용을 작성
-            //     TextFormField(
-            //       controller: _detailController,
-            //       maxLines: null,
-            //       keyboardType: TextInputType.multiline,
-            //       decoration: InputDecoration(
-            //         hintText: '올릴 게시글 내용을 작성해주세요',
-            //         contentPadding: const EdgeInsets.symmetric(horizontal: padding_16),
-            //         border: underLineBorder,
-            //         enabledBorder: underLineBorder,
-            //         focusedBorder: underLineBorder,
-            //       ),
-            //     ),
-            //   ],
-            // ),
+            // 컬럼으로 안하고 ListView 로 하는 이유는 스크롤 기능이 필요해서,
+            body: ListView(
+              children: <Widget>[
+                // 멀티 이미지 영역
+                const MultiImageSelect(),
+                dividerCustom,
+                // 제목영역
+                // TextFormField(
+                //   controller: _titleController,
+                //   decoration: InputDecoration(
+                //     hintText: '글제목',
+                //     contentPadding: const EdgeInsets.symmetric(horizontal: padding_16),
+                //     border: underLineBorder,
+                //     enabledBorder: underLineBorder,
+                //     focusedBorder: underLineBorder,
+                //   ),
+                // ),
+                // dividerCustom,
+                // // 카타고리 영역
+                // ListTile(
+                //   onTap: () {
+                //     debugPrint('/LOCATION_INPUT/LOCATION_CATEGORY_INPUT');
+                //     // context.beamToNamed('/$LOCATION_INPUT/$LOCATION_CATEGORY_INPUT');
+                //   },
+                //   dense: true,
+                //   title: const Text('카타고리'),
+                //   trailing: const Icon(Icons.navigate_next),
+                // ),
+                // dividerCustom,
+                // Row(
+                //   children: <Widget>[
+                //     Expanded(
+                //       child: Padding(
+                //         padding: const EdgeInsets.only(left: padding_16),
+                //         child: TextFormField(
+                //           keyboardType: TextInputType.number,
+                //           controller: _priceController,
+                //           onChanged: (value) {
+                //             if ('0 원' == value) {
+                //               _priceController.clear();
+                //             }
+                //             setState(() {});
+                //           },
+                //           inputFormatters: [
+                //             MoneyInputFormatter(mantissaLength: 0, trailingSymbol: ' 원')
+                //           ],
+                //           decoration: InputDecoration(
+                //             hintText: '가격',
+                //             prefixIcon: ImageIcon(
+                //               const ExtendedAssetImageProvider('assets/imgs/won.png'),
+                //               color:
+                //               (_priceController.text.isEmpty) ? Colors.grey[350] : Colors.black87,
+                //             ),
+                //             prefixIconConstraints: const BoxConstraints(maxWidth: 20),
+                //             contentPadding: const EdgeInsets.symmetric(vertical: padding_08),
+                //             border: underLineBorder,
+                //             enabledBorder: underLineBorder,
+                //             focusedBorder: underLineBorder,
+                //           ),
+                //         ),
+                //       ),
+                //     ),
+                //     Padding(
+                //       padding: const EdgeInsets.only(right: padding_16),
+                //       child: TextButton.icon(
+                //         onPressed: () {
+                //           setState(() {
+                //             _suggestPriceSelected = !_suggestPriceSelected;
+                //           });
+                //         },
+                //         icon: Icon(
+                //           _suggestPriceSelected ? Icons.check_circle : Icons.check_circle_outline,
+                //           color:
+                //           _suggestPriceSelected ? Theme.of(context).primaryColor : Colors.black54,
+                //         ),
+                //         label: Text(
+                //           '가격제안 받기',
+                //           style: TextStyle(
+                //             color:
+                //             _suggestPriceSelected ? Theme.of(context).primaryColor : Colors.black54,
+                //           ),
+                //         ),
+                //         style: TextButton.styleFrom(
+                //           backgroundColor: Colors.transparent,
+                //           primary: Colors.black45,
+                //         ),
+                //       ),
+                //     )
+                //   ],
+                // ),
+                // dividerCustom,
+                // 올릴 게시글 내용을 작성
+                // TextFormField(
+                //   controller: _detailController,
+                //   maxLines: null,
+                //   keyboardType: TextInputType.multiline,
+                //   decoration: InputDecoration(
+                //     hintText: '올릴 게시글 내용을 작성해주세요',
+                //     contentPadding: const EdgeInsets.symmetric(horizontal: padding_16),
+                //     border: underLineBorder,
+                //     enabledBorder: underLineBorder,
+                //     focusedBorder: underLineBorder,
+                //   ),
+                // ),
+              ],
+            ),
           ),
         );
       },
---
+++ lib/src/screens/input/multi_image_select.dart
@@ -0,0 +1,147 @@
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+
+import '../../constants/common_size.dart';
+
+class MultiImageSelect extends StatefulWidget {
+  const MultiImageSelect({Key? key}) : super(key: key);
+
+  @override
+  State<MultiImageSelect> createState() => _MultiImageSelectState();
+}
+
+class _MultiImageSelectState extends State<MultiImageSelect> {
+  bool _isPickingImages = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        // SelectImageNotifier selectImageNotifier = context.watch<SelectImageNotifier>();
+        Size _size = MediaQuery.of(context).size;
+        var imgSize = (_size.width / 3) - padding_16 * 2;
+
+        return SizedBox(
+          height: _size.width / 3,
+          // width: _size.width,
+          child: ListView(
+            // 기본 스크롤 방향을 위/아래 에서 좌/우로 변경해줌,
+            // 사이즈를 강제로 설정해야 함. 그래서 SizedBox 로 wrapping,
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Padding(
+                // 패딩 - 여기서는 전체를 16으로 처리해서 아래는 위/아래/오른쪽만 16 처리할 것,
+                padding: const EdgeInsets.all(padding_16),
+                child: InkWell(
+                  onTap: () async {
+                    // _isPickingImages = true;
+                    // setState(() {});
+
+                    // final ImagePicker _picker = ImagePicker();
+                    // // Pick multiple images
+                    // final List<XFile>? images = await _picker.pickMultiImage(imageQuality: 20);
+                    // if (images != null && images.isNotEmpty) {
+                    //   await context.read<SelectImageNotifier>().setNewImages(images);
+                    // }
+                    // _isPickingImages = false;
+                    // setState(() {});
+                  },
+                  child: Container(
+                    width: imgSize,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(padding_16),
+                      border: Border.all(color: Colors.grey, width: 1),
+                    ),
+// 첫번째 사진 아이콘 부분 구현
+                    child: _isPickingImages
+                        ? const Padding(
+                            padding: EdgeInsets.all(padding_16 * 2),
+                            child: CircularProgressIndicator(),
+                          )
+                        : Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Icon(Icons.camera_alt_rounded, color: Colors.grey),
+                              Text('0/10', style: Theme.of(context).textTheme.subtitle2),
+                            ],
+                          ),
+                  ),
+                ),
+              ),
+// 중복되는 사진 부분 구현
+// ... 으로 시작하면 기존 리스트에 새로운 결과(List.generate 결과)를 추가할 수 있다.
+              ...List.generate(
+                20,
+                (index) => Padding(
+                  // 패딩 - 위에서 전체를 16으로 처리해서 여기서는 위/아래/오른쪽만 16 처리할 것,
+                  padding:
+                      const EdgeInsets.only(right: padding_16, top: padding_16, bottom: padding_16),
+                  child: ExtendedImage.network(
+                    'https://picsum.photos/200',
+                    width: imgSize,
+                    height: imgSize,
+                    fit: BoxFit.cover,
+                    // shape 을 지정해야만 borderRadius 설정이 정상 동작함,
+                    borderRadius: BorderRadius.circular(padding_16),
+                    shape: BoxShape.rectangle,
+                  ),
+                ),
+              ),
+              // ...List.generate(10,
+              //   // selectImageNotifier.images.length,
+              //   // _images.length,
+              //       (index) => Stack(
+              //     children: <Widget>[
+              //       Padding(
+              //         padding: const EdgeInsets.only(
+              //             right: padding_16, top: padding_16, bottom: padding_16),
+              //         // future 타입을 변환해야 함.
+              //         child: ExtendedImage.memory(
+              //           // selectImageNotifier.images[index],
+              //           'https://picsum.photos/200',
+              //           width: imgSize,
+              //           height: imgSize,
+              //           fit: BoxFit.cover,
+              //           loadStateChanged: (state) {
+              //             switch (state.extendedImageLoadState) {
+              //               case LoadState.loading:
+              //                 return Container(
+              //                     width: imgSize,
+              //                     height: imgSize,
+              //                     padding: const EdgeInsets.all(padding_16 * 2),
+              //                     child: const CircularProgressIndicator());
+              //               case LoadState.completed:
+              //                 return null;
+              //               case LoadState.failed:
+              //                 return const Icon(Icons.cancel);
+              //             }
+              //           },
+              //           borderRadius: BorderRadius.circular(padding_16),
+              //           shape: BoxShape.rectangle,
+              //         ),
+              //       ),
+              //       Positioned(
+              //         top: 0,
+              //         right: 0,
+              //         width: 40,
+              //         height: 40,
+              //         child: IconButton(
+              //           padding: const EdgeInsets.all(8),
+              //           onPressed: () {
+              //             // selectImageNotifier.removeImage(index);
+              //           },
+              //           icon: const Icon(Icons.remove_circle),
+              //           color: Colors.black54,
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
